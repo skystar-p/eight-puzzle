df3c752c95bdfc44d3253b423b86ada0314385e1
An Dantae <48adt@naver.com>
2018-03-29 05:54:39 +0900

    Hansson's heuristic

--- eight_puzzle.py
+++ eight_puzzle.py
@@ -6,6 +6,8 @@ from random import randint, shuffle
 class Puzzle:
     def __init__(self, init, gen=0):
         self.grid = [[0 for _ in range(3)] for _ in range(3)]
+        self.sol = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+        self.solind = [0, 1, 2, 3, 4, 5, 6, 7, 8]
         for i in range(3):
             for j in range(3):
                 self.grid[i][j] = init[i][j]
@@ -73,43 +75,177 @@ class Puzzle:
         return c
 
     def is_solved(self):
-        sol = [1, 2, 3, 8, 0, 4, 7, 6, 5]
         for i in range(3):
             for j in range(3):
-                if self.grid[i][j] != sol[i * 3 + j]:
+                if self.grid[i][j] != self.sol[i * 3 + j]:
                     return False
         return True
 
-    def heuristic(self):
+    def conflict_r(self, t):
+        r = 0
+        if t == 0:
+            return r
+        for i in range(3):
+            k = -1
+            for j in range(3):
+                if self.grid[i][j] == t:
+                    k = j # column of tj
+                    break
+            if k >= 0:
+                l = -1
+                for j in range(3):
+                    if self.sol[3 * i + j] == t:
+                        l = j # column of the goal position of tj
+                        break
+                if l >= 0:
+                    for j in range(0, k):
+                        if self.grid[i][j] == 0:
+                            continue
+                        if (self.solind[self.grid[i][j]] // 3 == i) and (self.solind[self.grid[i][j]] % 3 > l):
+                            r += 1
+        return r
+    
+    def conflict_c(self, t):
+        r = 0
+        if t == 0:
+            return r
+        for j in range(3):
+            k = -1
+            for i in range(3):
+                if self.grid[i][j] == t:
+                    k = i # row of ti
+                    break
+            if k >= 0:
+                l = -1
+                for i in range(3):
+                    if self.sol[3 * i + j] == t:
+                        l = i # row of the goal position of ti
+                        break
+                if l >= 0:
+                    for i in range(0, k):
+                        if self.grid[i][j] == 0:
+                            continue
+                        if (self.solind[self.grid[i][j]] % 3 == j) and (self.solind[self.grid[i][j]] // 3 > l):
+                            r += 1
+        return r
+
+    def conflict_t(self, t1, t2):
+        if t1 == 0 or t2 == 0 or t1 == t2:
+            return False
+        i1 = -1
+        i2 = -1
+        j1 = -1
+        j2 = -1
+        gi1 = -1
+        gi2 = -1
+        gj1 = -1
+        gj2 = -1
+        for i in range(3):
+            for j in range(3):
+                if self.grid[i][j] == t1:
+                    i1 = i
+                    j1 = j
+                    gi1 = self.solind[t1] // 3
+                    gj1 = self.solind[t1] % 3
+                if self.grid[i][j] == t2:
+                    i2 = i
+                    j2 = j
+                    gi2 = self.solind[t2] // 3
+                    gj2 = self.solind[t2] % 3
+        if (i1 == i2 and i1 == gi1 and gi1 == gi2 and j1 > j2 and gj1 < gj2) or (j1 == j2 and j1 == gj1 and gj1 == gj2 and i1 > i2 and gi1 < gi2):
+            return True
+        return False
+
+
+    def md(self):
         h = 0
-        h2 = 0
-        sol = [1, 2, 3, 8, 0, 4, 7, 6, 5]
-        solind = [4, 0, 1, 2, 5, 8, 7, 6, 3]
         for i in range(3):
             for j in range(3):
                 s = self.grid[i][j]
-                k = solind[s]
+                k = self.solind[s]
                 if s == 0:
                     continue
                 h += (abs(k // 3 - i) + abs(k % 3 - j))
+        return h
         
-        if self.grid[1][1] != 0:
-            h2 += 1
-        l1 = [(self.grid[0][0], self.grid[0][1]), 
-                (self.grid[0][1], self.grid[0][2]),
-                (self.grid[0][2], self.grid[1][2]),
-                (self.grid[1][2], self.grid[2][2]),
-                (self.grid[2][2], self.grid[2][1]),
-                (self.grid[2][1], self.grid[2][0]),
-                (self.grid[2][0], self.grid[1][0])]
-        l2 = [(1, 2), (2, 3), (3, 4), (4, 5), (5, 6), (6, 7), (7, 8)]
-        for p in l1:
-            try:
-                l2.index(p)
-            except ValueError:
-                h2 += 2
-        h = h + 3 * h2
+
+    def heuristic(self):
+        h = 0
+        conflicts = []
         
+        for ri in range(3):
+            conflicts = []
+            tk = -1
+            tkc = 0
+            k = -1
+            for i in range(3):
+                conflicts.append(self.conflict_r(self.grid[ri][i]))
+                if tkc < conflicts[i]:
+                    tkc = conflicts[i]
+                    tk = self.grid[ri][i]
+                    k = i
+            if tkc == 0:
+                continue
+            conflicts[k] = 0
+            for i in range(3):
+                if self.conflict_t(self.grid[ri][i], tk):
+                    conflicts[i] -= 1
+            h += 1
+
+            tk = -1
+            tkc = 0
+            k = -1
+            for i in range(3):
+                if tkc < conflicts[i]:
+                    tkc = conflicts[i]
+                    tk = self.grid[ri][i]
+                    k = i
+            if tkc == 0:
+                continue
+            conflicts[k] = 0
+            for i in range(3):
+                if self.conflict_t(self.grid[ri][i], tk):
+                    conflicts[i] -= 1
+            h += 1
+
+        for ci in range(3):
+            conflicts = []
+            tk = -1
+            tkc = 0
+            k = -1
+            for i in range(3):
+                conflicts.append(self.conflict_c(self.grid[i][ci]))
+                if tkc < conflicts[i]:
+                    tkc = conflicts[i]
+                    tk = self.grid[i][ci]
+                    k = i
+            if tkc == 0:
+                continue
+            conflicts[k] = 0
+            for i in range(3):
+                if self.conflict_t(self.grid[i][ci], tk):
+                    conflicts[i] -= 1
+            h += 1
+
+            tk = -1
+            tkc = 0
+            k = -1
+            for i in range(3):
+                if tkc < conflicts[i]:
+                    tkc = conflicts[i]
+                    tk = self.grid[i][ci]
+                    k = i
+            if tkc == 0:
+                continue
+            conflicts[k] = 0
+            for i in range(3):
+                if self.conflict_t(self.grid[i][ci], tk):
+                    conflicts[i] -= 1
+            h += 1
+
+        h *= 2
+        h += self.md()
+
         return h + self.gen
 
 
@@ -120,7 +256,7 @@ def solve(puzzle):
 
     while True:
         curr = heapq.heappop(pq)
-        #print('Step {}, current heuristic is {} / {}'.format(steps, curr.heuristic(), curr.grid))
+        #print('Step {}, current heuristic is {}, md is {} / {}'.format(steps, curr.heuristic(), curr.md(), curr.grid))
         #print('Gen is {}'.format(curr.gen))
         if curr.is_solved():
             print('solved in {} steps; Gen is {}'.format(steps, curr.gen))
@@ -159,7 +295,7 @@ def solve(puzzle):
 
 
 def scramble():
-    g = [1, 2, 3, 8, 0, 4, 7, 6, 5]
+    g = [0, 1, 2, 3, 4, 5, 6, 7, 8]
     shuffle(g)
     c = 0
     gg = g[:]
@@ -169,7 +305,7 @@ def scramble():
             if gg[j] < gg[i]:
                 c += 1
 
-    if c % 2 == 0:
+    if c % 2 == 1:
         return scramble()
     l = [g[:3], g[3:6], g[6:]]
     return l
@@ -177,7 +313,7 @@ def scramble():
 
 gen_list = []
 step_list = []
-for _ in range(511):
+for _ in range(101):
     puz = Puzzle(scramble())
     s = solve(puz)
     step_list.append(s[0])
@@ -187,6 +323,12 @@ print("max of gen:  {}".format(max(gen_list)))
 print("avg of gen:  {}".format(sum(gen_list) / len(gen_list)))
 print("avg of step: {}".format(sum(step_list) / len(step_list)))
 """
-puz = Puzzle([[0, 1, 3], [8, 2, 4], [7, 6, 5]])
+puz = Puzzle([[2, 7, 0], [5, 4, 3], [8, 1, 6]])
 solve(puz)
+
+puz = Puzzle([[3, 4, 2], [0, 1, 5], [6, 7, 8]])
+print(puz.conflict_c(0))
+print(puz.conflict_c(3))
+print(puz.conflict_c(6))
+print(puz.conflict_c(1))
 """
